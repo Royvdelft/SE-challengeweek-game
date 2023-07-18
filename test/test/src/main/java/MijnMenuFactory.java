f7f8a3f1fe252981626b2cd2512f6c2e893ae1dc
Roy <royvandelft15@gmail.com>
2023-07-18 16:31:23 +0200

    removed comment, added NotNull annotation to get rid of some silly warning

--- test/test/src/main/java/MijnMenuFactory.java
+++ test/test/src/main/java/MijnMenuFactory.java
@@ -1,16 +1,13 @@
 import com.almasb.fxgl.app.scene.FXGLMenu;
 import com.almasb.fxgl.app.scene.MenuType;
 import com.almasb.fxgl.app.scene.SceneFactory;
+import org.jetbrains.annotations.NotNull;
 
 public class MijnMenuFactory extends SceneFactory {
 
+  @NotNull
   @Override
-  public FXGLMenu newMainMenu(){
+  public FXGLMenu newMainMenu() {
     return new MijnMenu(MenuType.MAIN_MENU);
   }
-
-//    @Override
-//    public FXGLMenu newGameMenu(){
-//        return new GameMenu(MenuType.GAME_MENU);
-//    }
 }
